6c3eb50c1da4e11664148405e507b60cde1ebcda
ALequinox <1397344525@qq.com>
2021-08-23 14:12:28 +0800

    Initial commit

---
+++ src/main/java/com/alw/teching_system/TechingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alw.teching_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alw/teching_system/TechingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alw.teching_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
