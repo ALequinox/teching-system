3feacfbf9d6c14f68812cdb1ec213995adde2ca9
ALequinox <1397344525@qq.com>
2021-08-24 10:30:38 +0800

    添加一个拦截器拦截未登录的请求

--- src/main/java/com/alw/teching_system/interceptor/LoginInterceptor.java
+++ src/main/java/com/alw/teching_system/interceptor/LoginInterceptor.java
@@ -8,12 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * 拦截所有请求，判断是否登录
+ */
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
         HttpSession session = request.getSession();
-        if(cookies != null){
+        if(!ObjectUtils.isEmpty(cookies)){
             for (Cookie cookie : cookies) {
                 if("UID".equals(cookie.getName())){
                     if (!ObjectUtils.isEmpty(session.getAttribute(cookie.getValue()))){
